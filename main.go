986f2fa4405470af939c7c71488c655520cf7c78
Domagoj Viljevac <dviljevacprivate@Domagojs-MacBook-Air-2.local>
2019-06-13 19:19:29 +0200

    Insert new offers

--- main.go
+++ main.go
@@ -15,12 +15,8 @@ import (
 // GLOBAL VARIABLE DECLARATION
 
 var LoadedLeagueListGlobal LigeList
-var OffersGlobal Ponude
+var OffersGlobal []Ponude
 
-/*********************************
-Definicija JSON elemenata liga
-
-********************************/
 type LigeList struct {
 	Lige []Lige `json:"lige"`
 }
@@ -36,18 +32,12 @@ type Tipovi struct {
 	Naziv string `json:"naziv"`
 }
 
-/*********************************
-Definicija JSON elemenata za vratiti lige
-********************************/
 type LeagueResp struct {
 	NazivLige   string `json:"nazivlige"`
 	PonudeULigi []int  `json:"ponudeuligi"`
 }
 
-/*********************************
-Definicija JSON elemenata ponuda
-********************************/
-type Ponude []struct {
+type Ponude struct {
 	Broj     string    `json:"broj"`
 	ID       int       `json:"id"`
 	Naziv    string    `json:"naziv"`
@@ -68,7 +58,7 @@ func main() {
 		db, err := sqlx.Connect("mysql", "root:root@tcp(localhost:3306)/mysql")
 
 		if err != nil {
-			log.Fatalln(err)
+			log.Println("Error occur. Action stopped\n",err)
 		}
 		db.MustExec("insert into login_log values ('login');")
 	*/
@@ -81,26 +71,26 @@ func main() {
 	http.ListenAndServe(":1234", nil)
 }
 
-func BetServerListener(w http.ResponseWriter, req *http.Request) {
-	// Use URL to decide what API call is triggered and call function
+func BetServerListener(w http.ResponseWriter, req *http.Request) { // Use URL to decide what API call is triggered and call function
 
 	apiCall := req.URL.Path[1:]
 	//	log.Println("New request arrived on path: " + apiCall)
-	if apiCall == "GetOffersPerLeagues" {
+	if apiCall == "GetOffersPerLeagues" { // davanje popisa liga na GET method zahtjev
 		log.Println("New request arrived on path: " + apiCall + " -> Server was asked to provide Leagues and offers for leagues")
 		lr := getLeaguesResp()
 		log.Println(lr)
 		resptext, err := json.Marshal(lr)
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error occur. Action stopped\n", err)
 		}
+		w.Header().Add("Content-Type", "application/json")
 		w.Write(resptext)
-	} else if apiCall == "GetOfferById" {
+	} else if apiCall == "GetOfferById" { //davanje ponude na GET method zahtjev
 		keys, ok := req.URL.Query()["offer_id"]
 		if !ok || len(keys[0]) < 1 { //ako ne postoji key ili je vrijednost kraca od 1 znak
 			log.Println("New request arrived on path: ", apiCall, " -> Server was asked to provide Offer but parameter offer_id is missing")
 			w.Write([]byte("offer_id value is missing"))
-		} else { //ako je key matchan i ima vrijednost
+		} else { //ako je key matchan i ima pozitivnu vrijednost
 			var oid int
 			oid, _ = strconv.Atoi(keys[0])
 			oid = getOfferByID(oid)
@@ -108,17 +98,64 @@ func BetServerListener(w http.ResponseWriter, req *http.Request) {
 				log.Println("New request arrived on path: ", apiCall, " -> Server was asked to provide Offer with ID:", keys[0], "/n", OffersGlobal[oid])
 				resptext, err := json.Marshal(OffersGlobal[oid])
 				if err != nil {
-					log.Fatal(err)
+					log.Println("Error occur. Action stopped\n", err)
 				}
+				w.Header().Add("Content-Type", "application/json")
 				w.Write(resptext)
 			} else {
 				log.Println("New request arrived on path: ", apiCall, " -> Server was asked to provide Offer with ID:", keys[0], " but it is invalid")
 				w.Write([]byte("offer_id value is invalid"))
 			}
 		}
-	} else if apiCall == "AddOffer" {
-		log.Println("New request arrived on path: " + apiCall + " -> Server was asked add new offer to list")
-		w.Write([]byte("Adding Offer"))
+	} else if apiCall == "AddOffer" { // dodavanje nove ponude na POST metodu s json bodyem
+		//log.Panicln(req.Method)
+		if req.Method != "POST" {
+			log.Println("New request arrived on path: " + apiCall + " that is not POST method.")
+			w.Write([]byte("Only POST method allowed"))
+		} else {
+			log.Println("New POST request arrived on path: " + apiCall + " -> Server was asked add new offer to list")
+			/*
+				//Ispis Headera
+				for name, value := range req.Header {
+					log.Println("", name, " : ", value)
+				}
+			*/
+
+			body, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				log.Println("Error occur. Action stopped\n", err)
+			} else if req.Header.Get("Content-Type") == "application/json" {
+				//log.Println("POST body is json:\n", string(body))
+				var P []Ponude
+				err := json.Unmarshal(body, &P)
+				if err != nil {
+					var Po Ponude
+					err2 := json.Unmarshal(body, &Po)
+					if err2 != nil {
+						log.Println(err2)
+					}
+					P = append(P, Po)
+				}
+				//log.Println("POST body is json:\n" + string(body))
+				//log.Println(P)
+
+				//log.Println(provjeraPopisaPonuda())
+				cnt := 0
+				for i := 0; i < len(P); i++ {
+					if !checkExistingOffer(P[i].ID) {
+						OffersGlobal = append(OffersGlobal, P[i])
+						cnt++
+					}
+				}
+				//log.Println(provjeraPopisaPonuda())
+				log.Println("POST body contains", len(P), "offers. Added", cnt, "offers to existing list")
+
+				w.Write([]byte("AddOffer"))
+			} else {
+				log.Println("POST body is not json\n", string(body))
+				w.Write([]byte("No json file in POST body"))
+			}
+		}
 	} else if apiCall == "NewTicket" {
 		log.Println("New request arrived on path: " + apiCall + " -> New ticket received")
 		w.Write([]byte("Verifying ticket"))
@@ -134,7 +171,7 @@ func getLeaguesFromServerHTTP(u string) { // Funkcija kojom dohvacam lige.json
 	log.Println("Sending request to: " + u)
 	resp, err := http.Get(u)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error occur. Action stopped\n", err)
 	}
 	defer resp.Body.Close()
 
@@ -154,10 +191,10 @@ func getLeaguesFromServerHTTP(u string) { // Funkcija kojom dohvacam lige.json
 
 		err := json.Unmarshal(body, &LoadedLeagueListGlobal)
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error occur. Action stopped\n", err)
 		}
 		//log.Println(leagueJSON)
-		log.Println("JSON received and parsed:")
+		log.Println("lige.json received and parsed. Leagues loaded:", len(LoadedLeagueListGlobal.Lige))
 		/*
 			for i := 0; i < len(leagueJSON.Lige); i++ {
 
@@ -191,7 +228,7 @@ func getOffersFromServerHTTP(u string) { // Funkcija kojom dohvacam ponude.json
 	log.Println("Sending request to: " + u)
 	resp, err := http.Get(u)
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Error occur. Action stopped\n", err)
 	}
 	defer resp.Body.Close()
 
@@ -200,9 +237,9 @@ func getOffersFromServerHTTP(u string) { // Funkcija kojom dohvacam ponude.json
 	if resp.Header.Get("Content-Type") == "application/json" {
 		err := json.Unmarshal(body, &OffersGlobal)
 		if err != nil {
-			log.Fatal(err)
+			log.Println("Error occur. Action stopped\n", err)
 		}
-		log.Println("ponude.json received and parsed. Overall", len(OffersGlobal), " offer loaded")
+		log.Println("ponude.json received and parsed. Overall", len(OffersGlobal), "offer loaded")
 	}
 }
 func getOfferByID(id int) int {
@@ -210,8 +247,26 @@ func getOfferByID(id int) int {
 	for i := 0; i < len(OffersGlobal); i++ {
 		if OffersGlobal[i].ID == id {
 			return i
-			break
 		}
 	}
 	return -1
+}
+func checkExistingOffer(newId int) bool {
+	for i := 0; i < len(OffersGlobal); i++ {
+		if OffersGlobal[i].ID == newId {
+			return true
+		}
+	}
+	return false
+}
+func provjeraPopisaPonuda() string {
+	str := ""
+	for i := 0; i < len(OffersGlobal); i++ {
+		if i == 0 {
+			str = strconv.Itoa(OffersGlobal[i].ID)
+		} else {
+			str += ", " + strconv.Itoa(OffersGlobal[i].ID)
+		}
+	}
+	return str
 }
